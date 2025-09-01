1e5be85854613ca25619289f59ef46d33b953fd9
MkBaek0229 <alsrl6678@naver.com>
2025-09-01 12:08:41 +0900

    헤더 수정 및, 체크페이지 퍼블리싱 작업중

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -4,9 +4,22 @@ module.exports = {
   theme: {
 
     extend: {
-        colors: {
-         'sig-color': '#0202FF',
-        }
+     keyframes: {
+            animate: {
+                  '0%':   { backgroundPosition: '0 100px' },
+                  '10%':  { backgroundPosition: '0 100px' },
+                  '40%':  { backgroundPosition: '1000px -70px' },
+                  '80%':  { backgroundPosition: '2000px -80px' },
+                  '100%': { backgroundPosition: '2500px 100px' },
+            },
+          },
+          animation: {
+            animate: 'animate 10s linear infinite',
+          },
+
+          colors: {
+            'sig-color': '#0202FF',
+          }
     },
   },
   plugins: [],
